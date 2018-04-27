acdeaf8189d429bfd885f6a28f28b815609afea9
Viktor Smari <viktorsmari@gmail.com>
2018-04-27 15:59:31 +0200

    Parse each sensor

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import json
 from urllib.request import Request, urlopen
 
-url = "https://api.smartcitizen.me/v0/devices/2440"
+url = "https://api.smartcitizen.me/v0/devices/2300"
 req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 
 data_bytes = urlopen(req).read()
@@ -15,4 +15,14 @@ print(data_json['id'])
 print(data_json['state'])
 print(data_json['kit']['updated_at'])
 print(data_json['owner']['location']['city'])
+
+sensors = data_json['data']['sensors']
+
+for i in sensors:
+    if i['id'] == 29:
+        print('found id 29')
+    if i['id'] == 16:
+        print('found id 16')
+
+#print(sensors)
 

80c485151ae8b65e93197ad8fac753bd0ad172a1
Viktor Smari <viktorsmari@gmail.com>
2018-05-03 13:57:48 +0200

    Apply special rules

--- script.py
+++ script.py
@@ -1,52 +1,68 @@
 import json
 from urllib.request import Request, urlopen
 
-# 1. Fetch rules into a json variable
-with open('./rules.json', 'r') as f:
-    rules = json.load(f)
+# 1. Fetch default_rules into a json variable
+with open('./default_rules.json', 'r') as f:
+    default_rules = json.load(f)
 
-# 2. Fetch new data as json
+with open('./special_rules.json', 'r') as f:
+    special_rules = json.load(f)
+
+# 2. Fetch all devices from the API
+#device_list = [2300, 2301, 2304]
 device_list = [2300, 2301]
-url = "https://api.smartcitizen.me/v0/devices/2300"
-req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-
-data_bytes = urlopen(req).read()
-data_string = data_bytes.decode('utf8')
-api_json = json.loads(data_string)
-
-# Clean / remove unwanted data
-api_json.pop('kit', None)
-api_json.pop('owner', None)
-api_json.pop('system_tags', None)
-api_json.pop('user_tags', None)
-api_json.pop('last_reading_at', None)
-api_json.pop('state', None)
-api_json.pop('mac_address', None)
-api_json.pop('last_reading_at', None)
-for sens in api_json['data']['sensors']:
-    sens.pop('ancestry', None)
-    sens.pop('created_at', None)
-    sens.pop('description', None)
-    sens.pop('measurement_id', None)
-    sens.pop('name', None)
-    sens.pop('prev_raw_value', None)
-    sens.pop('prev_value', None)
-    sens.pop('unit', None)
-    sens.pop('uuid', None)
-
 final = {}
+final['devices'] = []
+
+for dev in device_list:
+    url = "https://api.smartcitizen.me/v0/devices/%s" % dev
+
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+
+    data_bytes = urlopen(req).read()
+    data_string = data_bytes.decode('utf8')
+    api_json = json.loads(data_string)
+
+    # Clean / remove unwanted data
+    api_json.pop('kit', None)
+    api_json.pop('owner', None)
+    api_json.pop('system_tags', None)
+    api_json.pop('user_tags', None)
+    api_json.pop('last_reading_at', None)
+    api_json.pop('state', None)
+    api_json.pop('mac_address', None)
+    api_json.pop('last_reading_at', None)
+    for sens in api_json['data']['sensors']:
+        sens.pop('ancestry', None)
+        sens.pop('created_at', None)
+        sens.pop('description', None)
+        sens.pop('measurement_id', None)
+        sens.pop('name', None)
+        sens.pop('prev_raw_value', None)
+        sens.pop('prev_value', None)
+        sens.pop('unit', None)
+        sens.pop('uuid', None)
+    final['devices'].append(api_json)
 
 # 3. Add each device to the final json
-final['devices'] = [api_json, api_json]
 
-def get_rule(key, sensorid):
-    for i in rules:
-        if i['id'] == sensorid:
-            return i[key]
+# FUNCTIONS
 
-def change_key_with_value(key, value):
-    print('changing sensor id: %s with value %f' % (key, value))
-    print('-----')
+def get_rule(deviceid, key, sensorid):
+    for i in special_rules:
+        if deviceid == i['device_id']:
+            print('SPECIAL')
+            for i in special_rules:
+                if i['id'] == sensorid:
+                    return i[key]
+        else:
+            print('NOT SPECIAL')
+            for i in default_rules:
+                if i['id'] == sensorid:
+                    return i[key]
+
+def update_key_with_value(key, value):
+    print('Normalizing sensor id: %s with value %f' % (key, value))
     for device in final['devices']:
         for sensor in device['data']['sensors']:
             if sensor['id'] == key:
@@ -54,25 +70,27 @@ def change_key_with_value(key, value):
                 sensor['value'] = value
 
 # 4. Extract data
-for i in api_json['data']['sensors']:
-    if i['id'] == 12:
-        #print('air temp')
-        change_key_with_value(i['id'], 0.22)
-    if i['id'] == 13:
-        #print('humidity')
-        change_key_with_value(i['id'], 0.33)
-    if i['id'] == 14:
-        #print('light')
-        change_key_with_value(i['id'], 0.44)
-    if i['id'] == 16:
-        #print('car exhaust')
-        #print(get_rule('low', i['id']) * i['raw_value'] )
-        #print(get_rule('high',i['id']) * 10 )
-        change_key_with_value(i['id'], 0.66)
-    if i['id'] == 29:
-        #print('noise data')
-        change_key_with_value(i['id'], 0.99)
-
+for device in final['devices']:
+    for i in device['data']['sensors']:
+        if i['id'] == 12:
+            #print('air temp')
+            # Apply correct rule
+            update_key_with_value(i['id'], 0.22222)
+        if i['id'] == 13:
+            #print('humidity')
+            update_key_with_value(i['id'], 0.33333)
+        if i['id'] == 14:
+            #print('light')
+            update_key_with_value(i['id'], 0.44444)
+        if i['id'] == 16:
+            #print('car exhaust')
+            #print(get_rule(device['id'], 'low', i['id']) * i['raw_value'] )
+            tmp_val = get_rule(device['id'], 'high', i['id'])
+            print('tmp_val', tmp_val)
+            update_key_with_value(i['id'], 0.66666)
+        if i['id'] == 29:
+            #print('noise data')
+            update_key_with_value(i['id'], 0.99999)
 
 
 #print(final)
